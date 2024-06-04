1d367f712457b5fae137ceb3f4f705eb52b95660
SimpleShun <rahulgurung051@gmail.com>
2024-06-04 21:25:49 +0545

    did things

--- things/csv/parser.js
+++ things/csv/parser.js
@@ -1,22 +1,22 @@
 import fs from 'fs'
 import { parse } from 'csv-parse'
-import neatCsv from 'neat-csv'
 
-const result = []
-
-// async function getCsv() {
-// 	let res = await neatCsv('./pg_catalog.csv')
-// 	let res2 = []
-// 	for (const i of res) {
-// 		if (i['Text#'] === 2) {
-// 			res2.push(i);
-// 		}
-// 	}
-// 	return res2
-// }
-//
-
-
-export const parsecsv = () => {
-	return getCsv()
+const path = import.meta.dirname.concat('/pg_catalog.csv')
+export default async function csv_parser(id) {
+	let records = {}
+	const parsed = fs.createReadStream(path)
+		.pipe(parse());
+	for await (const record of parsed) {
+		if (record[0] == id) {
+			records = {
+				webver: `https://www.gutenberg.org/cache/epub/${record[0]}/pg${record[0]}-images.html`,
+				textver: `https://www.gutenberg.org/cache/epub/${record[0]}/pg${record[0]}.txt`,
+				imglink: `https://www.gutenberg.org/cache/epub/${record[0]}/pg${record[0]}.cover.medium.jpg `,
+				name: `${record[3]}`,
+				genre: `${record[6]}`,
+				lang: `${record[4]}`,
+			}
+			return records
+		}
+	}
 }
--- things/items/items_router.js
+++ things/items/items_router.js
@@ -1,11 +1,15 @@
-import { parsecsv } from '../csv/parser.js'
+import csv_parser from '../csv/parser.js'
 import { Router } from 'express'
 
 export const router = Router()
 
 router.get('/', async (req, res) => {
-	let result = await parsecsv();
-	console.log(result)
+	const result = await csv_parser(Math.floor(Math.random() * 5000))
+	res.json(result)
+})
+
+router.get('/:id', async (req, res) => {
+	const result = await csv_parser(req.params.id)
 	res.json(result)
 })
 
